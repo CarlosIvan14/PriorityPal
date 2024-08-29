e8a0f8d04cfe18317496cc3aecb3220d1a4485b6
mrcolor-blind <mrcolor-blind@users.noreply.github.com>
2024-08-28 23:15:22 -0600

    Page 10 showing

--- App.tsx
+++ App.tsx
@@ -1,13 +1,8 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-//Paginas 10, 11, 12
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import Header from './components/header';
+import Page10 from './components/page10';
+
 
 const styles = StyleSheet.create({
   container: {
@@ -25,44 +20,9 @@ export default function App() {
   return (
     <View style={styles.container}> 
       <Header />
+      <Page10/>
       <Text>perro</Text> 
+
     </View>
   );
 }
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <View style={styles.sectionContainer}>
-      <Text>Hello</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    width: '100%',
-    height: '100%',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
